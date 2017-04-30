09cfa29ab984a9d398fb995820d88afb21d92453
Jeff Shen <jshen@protonmail.ch>
2017-04-30 01:10:54 -0700

    Added icons, better CSS, body added

---
+++ src/Body.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Footer from './Footer';
+
+class Body extends Component {
+
+  render() {
+    return (
+      <div className='body'>
+        <div className='title'>
+          
+        </div>
+        
+        <div className='bodyText'>
+          
+        </div>
+        
+        <div className='bodyFooter'>
+          <Footer />
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default Body;
